950138ee0f21cb86798b504d30604cd9c2a5ddbb
Theo F <theo.farnole@gmail.com>
2023-06-14 21:01:33 +0200

    Initial commit

---
+++ Editor/AssetsCreatorWindow.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace TF.AssetsCreator
+{
+
+    public class AssetsCreatorWindow : EditorWindow
+    {
+        [SerializeField] public string targetFolder;
+        [SerializeField] private HashSet<Type> _scriptablesTypes;
+        [SerializeField] private Vector2 _position;
+
+        private ScriptableObject previewObject;
+        private Editor previewObjectEditor;
+
+
+        [MenuItem("Assets/Create Scriptable Object...", priority = -10000)]
+        private static void ShowDialog()
+        {
+            var path = "Assets";
+            var obj = Selection.activeObject;
+            if (obj && AssetDatabase.Contains(obj))
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+                if (!Directory.Exists(path))
+                {
+                    path = Path.GetDirectoryName(path);
+                }
+            }
+
+            var window = CreateInstance<AssetsCreatorWindow>();
+            window.ShowUtility();
+            window.titleContent = new GUIContent(path);
+            window.targetFolder = path;
+        }
+
+        private void OnEnable()
+        {
+            SetScriptables();
+        }
+
+        private void SetScriptables()
+        {
+            _scriptablesTypes = AssemblyUtilities.GetUserScriptableObjectTypes();
+        }
+
+        private void OnGUI()
+        {
+            if (_scriptablesTypes == null)
+                SetScriptables();
+
+
+            GUILayout.BeginHorizontal();
+            {
+                _position = GUILayout.BeginScrollView(_position, GUILayout.Width(300));
+                {
+                    EditorGUILayout.LabelField("Select a scriptable object", EditorStyles.boldLabel);
+
+                    foreach (var type in _scriptablesTypes)
+                    {
+                        string name = ObjectNames.NicifyVariableName(type.Name);
+
+                        if (GUILayout.Button(name))
+                        {
+                            previewObject = CreateInstance(type);
+                            previewObjectEditor = Editor.CreateEditor(previewObject);
+                            //CreateAsset(type, type.Name);
+                        }
+                    }
+                }
+                GUILayout.EndScrollView();
+
+                GUILayout.BeginVertical();
+                {
+
+                    if (previewObject)
+                    {
+                        previewObjectEditor.OnInspectorGUI();
+
+                        GUILayout.FlexibleSpace();
+                        if (GUILayout.Button("Create Asset", GUILayout.Height(30)))
+                        {
+                            CreateAsset();
+                        }
+                    }
+                }
+                GUILayout.EndVertical();
+            }
+            GUILayout.EndHorizontal();
+        }
+
+        private void CreateAsset()
+        {
+            if (!previewObject) return;
+
+            var dest = targetFolder + "/" + name + ".asset";
+            dest = AssetDatabase.GenerateUniqueAssetPath(dest);
+            ProjectWindowUtil.CreateAsset(previewObject, dest);
+            EditorApplication.delayCall += this.Close;
+        }
+    }
+}
---
+++ Editor/Utilities/AssemblyUtilities.cs
@@ -0,0 +1,199 @@
+﻿namespace TF.AssetsCreator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using UnityEditor;
+    using UnityEngine;
+
+    public static class AssemblyUtilities
+    {
+        public static HashSet<Type> GetUserScriptableObjectTypes()
+        {
+            return GetScriptableObjectTypes()
+            .Where(t => t.Namespace == null || t.Namespace != null && t.Namespace.StartsWith("Unity") == false)
+            .ToHashSet();
+        }
+
+        public static HashSet<Type> GetScriptableObjectTypes()
+        {
+            return AppDomain.CurrentDomain.GetUserCreatedAssemblies()
+            .SelectMany(x => x.GetTypes())
+             .Where(t =>
+                 t.IsClass &&
+                 !t.IsAbstract &&
+                 typeof(ScriptableObject).IsAssignableFrom(t) &&
+                 !typeof(EditorWindow).IsAssignableFrom(t) &&
+                 !typeof(UnityEditor.Editor).IsAssignableFrom(t))
+             .ToHashSet();
+        }
+
+        private static List<Type> GetAllTypesInAssembly(string[] pAssemblyNames)
+        {
+            List<Type> results = new List<Type>();
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (string assemblyName in pAssemblyNames)
+                {
+                    if (assembly.FullName.StartsWith(assemblyName))
+                    {
+                        foreach (Type type in assembly.GetTypes())
+                        {
+                            results.Add(type);
+                        }
+                        break;
+                    }
+                }
+            }
+            return results;
+        }
+
+        private static IEnumerable<Assembly> GetUserCreatedAssemblies(this AppDomain appDomain)
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                if (assembly.IsDynamic)
+                {
+                    continue;
+                }
+
+                string assemblyName = assembly.GetName().Name;
+                if (internalAssemblyNames.Contains(assemblyName))
+                {
+                    continue;
+                }
+
+                yield return assembly;
+            }
+        }
+
+        private static readonly HashSet<string> internalAssemblyNames = new HashSet<string>()
+{
+    "mscorlib",
+    "System",
+    "System.Core",
+    "System.Security.Cryptography.Algorithms",
+    "System.Net.Http",
+    "System.Data",
+    "System.Runtime.Serialization",
+    "System.Xml.Linq",
+    "System.Numerics",
+    "System.Xml",
+    "System.Configuration",
+    "ExCSS.Unity",
+    "Unity.Cecil",
+    "Unity.CompilationPipeline.Common",
+    "Unity.SerializationLogic",
+    "Unity.TestTools.CodeCoverage.Editor",
+    "Unity.ScriptableBuildPipeline.Editor",
+    "Unity.Addressables.Editor",
+    "Unity.ScriptableBuildPipeline",
+    "Unity.CollabProxy.Editor",
+    "Unity.Timeline.Editor",
+    "Unity.PerformanceTesting.Tests.Runtime",
+    "Unity.Settings.Editor",
+    "Unity.PerformanceTesting",
+    "Unity.PerformanceTesting.Editor",
+    "Unity.Rider.Editor",
+    "Unity.ResourceManager",
+    "Unity.TestTools.CodeCoverage.Editor.OpenCover.Mono.Reflection",
+    "Unity.PerformanceTesting.Tests.Editor",
+    "Unity.TextMeshPro",
+    "Unity.Timeline",
+    "Unity.Addressables",
+    "Unity.TestTools.CodeCoverage.Editor.OpenCover.Model",
+    "Unity.VisualStudio.Editor",
+    "Unity.TextMeshPro.Editor",
+    "Unity.VSCode.Editor",
+    "UnityEditor",
+    "UnityEditor.UI",
+    "UnityEditor.TestRunner",
+    "UnityEditor.CacheServer",
+    "UnityEditor.WindowsStandalone.Extensions",
+    "UnityEditor.Graphs",
+    "UnityEditor.UnityConnectModule",
+    "UnityEditor.UIServiceModule",
+    "UnityEditor.UIElementsSamplesModule",
+    "UnityEditor.UIElementsModule",
+    "UnityEditor.SceneTemplateModule",
+    "UnityEditor.PackageManagerUIModule",
+    "UnityEditor.GraphViewModule",
+    "UnityEditor.CoreModule",
+    "UnityEngine",
+    "UnityEngine.UI",
+    "UnityEngine.XRModule",
+    "UnityEngine.WindModule",
+    "UnityEngine.VirtualTexturingModule",
+    "UnityEngine.TestRunner",
+    "UnityEngine.VideoModule",
+    "UnityEngine.VehiclesModule",
+    "UnityEngine.VRModule",
+    "UnityEngine.VFXModule",
+    "UnityEngine.UnityWebRequestWWWModule",
+    "UnityEngine.UnityWebRequestTextureModule",
+    "UnityEngine.UnityWebRequestAudioModule",
+    "UnityEngine.UnityWebRequestAssetBundleModule",
+    "UnityEngine.UnityWebRequestModule",
+    "UnityEngine.UnityTestProtocolModule",
+    "UnityEngine.UnityCurlModule",
+    "UnityEngine.UnityConnectModule",
+    "UnityEngine.UnityAnalyticsModule",
+    "UnityEngine.UmbraModule",
+    "UnityEngine.UNETModule",
+    "UnityEngine.UIElementsNativeModule",
+    "UnityEngine.UIElementsModule",
+    "UnityEngine.UIModule",
+    "UnityEngine.TilemapModule",
+    "UnityEngine.TextRenderingModule",
+    "UnityEngine.TextCoreModule",
+    "UnityEngine.TerrainPhysicsModule",
+    "UnityEngine.TerrainModule",
+    "UnityEngine.TLSModule",
+    "UnityEngine.SubsystemsModule",
+    "UnityEngine.SubstanceModule",
+    "UnityEngine.StreamingModule",
+    "UnityEngine.SpriteShapeModule",
+    "UnityEngine.SpriteMaskModule",
+    "UnityEngine.SharedInternalsModule",
+    "UnityEngine.ScreenCaptureModule",
+    "UnityEngine.RuntimeInitializeOnLoadManagerInitializerModule",
+    "UnityEngine.ProfilerModule",
+    "UnityEngine.Physics2DModule",
+    "UnityEngine.PhysicsModule",
+    "UnityEngine.PerformanceReportingModule",
+    "UnityEngine.ParticleSystemModule",
+    "UnityEngine.LocalizationModule",
+    "UnityEngine.JSONSerializeModule",
+    "UnityEngine.InputLegacyModule",
+    "UnityEngine.InputModule",
+    "UnityEngine.ImageConversionModule",
+    "UnityEngine.IMGUIModule",
+    "UnityEngine.HotReloadModule",
+    "UnityEngine.GridModule",
+    "UnityEngine.GameCenterModule",
+    "UnityEngine.GIModule",
+    "UnityEngine.DirectorModule",
+    "UnityEngine.DSPGraphModule",
+    "UnityEngine.CrashReportingModule",
+    "UnityEngine.CoreModule",
+    "UnityEngine.ClusterRendererModule",
+    "UnityEngine.ClusterInputModule",
+    "UnityEngine.ClothModule",
+    "UnityEngine.AudioModule",
+    "UnityEngine.AssetBundleModule",
+    "UnityEngine.AnimationModule",
+    "UnityEngine.AndroidJNIModule",
+    "UnityEngine.AccessibilityModule",
+    "UnityEngine.ARModule",
+    "UnityEngine.AIModule",
+    "SyntaxTree.VisualStudio.Unity.Bridge",
+    "nunit.framework",
+    "Newtonsoft.Json",
+    "ReportGeneratorMerged",
+    "Unrelated",
+    "netstandard",
+    "SyntaxTree.VisualStudio.Unity.Messaging"
+};
+    }
+}
